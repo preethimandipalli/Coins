55298a15b27b7be67b7f175a726287d54f49928c
preethi <preethimandipalli99@gmail.com>
2021-04-06 07:39:57 +0530

    added functionality to check if probabilities are equal

--- src/test/java/utility/TestCoinFlipping.java
+++ src/test/java/utility/TestCoinFlipping.java
@@ -1,5 +1,20 @@
 package utility;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestCoinFlipping {
+    @Test
+    void TestToCheckIfProbabilityOfHeadIsEqualToThatOfTail(){
+        int count=1;
+        Coins coin=new Coins(count);
+        boolean expected=true;
+
+        boolean actual=coin.checkEqualityOfProbability();
+
+        assertEquals(expected,actual);
+
+    }
 
 }
